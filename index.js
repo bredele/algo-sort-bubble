e0dffc4916c950612d67bcbe06012b1654011178
bredele <olivier.wietrich@gmail.com>
2017-04-08 15:58:02 -0600

    optimize algorithm

--- index.js
+++ index.js
@@ -12,15 +12,17 @@ module.exports = function(array) {
   var length = array.length
   while(length) {
     var i = 0
-    var bool = true
+    var sorted = true
     while(i < (length - 1)) {
       if(array[i + 1] < array[i]) {
         var tmp = array[i + 1]
         array[i + 1] = array[i]
         array[i] = tmp
+        sorted = false
       }
       i++
     }
+    if(sorted) return array
     length--
   }
   return array
