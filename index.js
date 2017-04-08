3e2cf1282a8a82446be45424526c9c80d312d285
bredele <olivier.wietrich@gmail.com>
2017-04-08 15:55:41 -0600

    added code documentation

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 
 
 /**
+ * Sort array using bubble sort algorithm.
  *
+ * @param {Array} array
+ * @return {Array}
+ * @api public
  */
 
 module.exports = function(array) {
   var length = array.length
   while(length) {
     var i = 0
+    var bool = true
     while(i < (length - 1)) {
       if(array[i + 1] < array[i]) {
         var tmp = array[i + 1]
