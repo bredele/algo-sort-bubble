1e034f59247d9304ec40b3824887da648ec4a46d
bredele <olivier.wietrich@gmail.com>
2017-04-08 19:47:51 -0600

    refactor algorithm

--- index.js
+++ index.js
@@ -11,21 +11,19 @@
 module.exports = function(array, distance) {
   distance = distance || ((prev, next) => prev < next)
   array = array.slice(0)
-  var length = array.length
-  while(length) {
-    var i = 0
-    var sorted = true
-    while(i < (length - 1)) {
-      if(distance(array[i + 1], array[i])) {
-        var tmp = array[i + 1]
-        array[i + 1] = array[i]
-        array[i] = tmp
-        sorted = false
+  var length = array.length - 1
+  var swapped
+  do {
+    swapped = false
+    for(var i = 0; i < length; i++) {
+      var big = array[i]
+      var small = array[i+1]
+      if(distance(small, big)) {
+        array[i] = small
+        array[i + 1] = big
+        swapped = true
       }
-      i++
     }
-    if(sorted) return array
-    length--
-  }
+  } while (swapped)
   return array
 }
